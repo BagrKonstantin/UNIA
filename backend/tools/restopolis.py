c75b85faa7e1e8c3ea00a4ff3970f8eb3bfec0fe
kbagrian <kbagrian@student.42luxembourg.lu>
2026-04-26 23:14:52 +0200

    Changed prompt a little bit

--- backend/tools/restopolis.py
+++ backend/tools/restopolis.py
@@ -123,22 +123,24 @@ def get_canteen_menu(location: str = "belval", date: str = datetime.today().strf
     """
     Get the menu for a campus canteen (e.g. food in 'Belval', 'Kirchberg', 'Limpertsberg') for a specific date in '%d.%m' format.
     Returns the list of available meals.
-    "1": "Céréales contenant du gluten (Blé, Seigle, Orge, Avoine, Épeautre, Kamut)",
-    "2": "Crustacés",
-    "3": "Œufs",
-    "4": "Poissons",
-    "5": "Arachides",
-    "6": "Soja",
-    "7": "Lait (Lactose)",
-    "8": "Fruits à coque (Amandes, Noisettes, Noix, Pistaches, etc.)",
-    "9": "Céleri",
-    "10": "Moutarde",
-    "11": "Graines de sésame",
-    "12": "Anhydride sulfureux et sulfites (>10mg/kg)",
-    "13": "Lupin",
-    "14": "Mollusques"
+
     ALLERGEN POLICY: DO NOT mention allergens in your response if the user didn't ask about them. 
-    If the user states they have an allergy, you MUST use the returned allergens string and match their allergies with this allergen dictionary: 
+    If the user states they have an allergy, you MUST use the returned allergens string and match their allergies with this allergen dictionary:
+
+    1: Céréales contenant du gluten (Blé, Seigle, Orge, Avoine, Épeautre, Kamut),
+    2: Crustacés,
+    3: Œufs,
+    4: Poissons,
+    5: Arachides,
+    6: Soja,
+    7: Lait (Lactose),
+    8: Fruits à coque (Amandes, Noisettes, Noix, Pistaches, etc.),
+    9: Céleri,
+    10: Moutarde,
+    11: Graines de sésame,
+    12: Anhydride sulfureux et sulfites (>10mg/kg),
+    13: Lupin,
+    14: Mollusques
 
     Then completely filter out and DO NOT mention meals that the user cannot eat.
     """
