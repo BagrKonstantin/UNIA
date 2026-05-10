8e743de6eb29f938420461a5329c5b02d66516f1
kbagrian <kbagrian@student.42luxembourg.lu>
2026-05-10 13:16:16 +0200

    changed date for current week

--- backend/load_data.py
+++ backend/load_data.py
@@ -3,7 +3,7 @@ import json
 from backend.parcers.events import get_events_for_this_month
 from backend.parcers.restopolis import scrape_all_campus_menus
 
-full_menu_data = scrape_all_campus_menus("2026-04-27")
+full_menu_data = scrape_all_campus_menus("2026-05-11")
 with open('data/campus_menus.json', 'w', encoding='utf-8') as f:
     json.dump(full_menu_data, f, ensure_ascii=False, indent=4)
 
