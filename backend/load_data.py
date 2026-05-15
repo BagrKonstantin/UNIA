19ba3101b02df8bcb618dc205cb81db2734d757e
kbagrian <kbagrian@student.42luxembourg.lu>
2026-05-15 10:55:58 +0200

    Added workshops preload

--- backend/load_data.py
+++ backend/load_data.py
@@ -2,6 +2,7 @@ import json
 
 from backend.parcers.events import get_events_for_this_month
 from backend.parcers.restopolis import scrape_all_campus_menus
+from backend.parcers.workshops import request_workshops
 
 full_menu_data = scrape_all_campus_menus("2026-05-11")
 with open('data/campus_menus.json', 'w', encoding='utf-8') as f:
@@ -9,4 +10,8 @@ with open('data/campus_menus.json', 'w', encoding='utf-8') as f:
 
 events = get_events_for_this_month()
 with open('data/events.json', 'w', encoding='utf-8') as f:
+    json.dump(events, f, ensure_ascii=False, indent=4)
+
+workshops = request_workshops()
+with open("data/workshops.json", 'w', encoding='utf-8') as f:
     json.dump(events, f, ensure_ascii=False, indent=4)
