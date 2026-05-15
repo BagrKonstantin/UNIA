a881e0d16ceccee773dff0bed5389d77bdfcda11
kbagrian <kbagrian@student.42luxembourg.lu>
2026-05-15 10:56:26 +0200

    Updated date for canteen menu preload

--- backend/load_data.py
+++ backend/load_data.py
@@ -4,7 +4,7 @@ from backend.parcers.events import get_events_for_this_month
 from backend.parcers.restopolis import scrape_all_campus_menus
 from backend.parcers.workshops import request_workshops
 
-full_menu_data = scrape_all_campus_menus("2026-05-11")
+full_menu_data = scrape_all_campus_menus("2026-05-18")
 with open('data/campus_menus.json', 'w', encoding='utf-8') as f:
     json.dump(full_menu_data, f, ensure_ascii=False, indent=4)
 
