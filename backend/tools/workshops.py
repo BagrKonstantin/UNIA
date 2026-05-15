c2b7c3e9d00cda990041f9b734d9fb81182cdb26
kbagrian <kbagrian@student.42luxembourg.lu>
2026-05-15 11:22:11 +0200

    fixed path to file

--- backend/tools/workshops.py
+++ backend/tools/workshops.py
@@ -2,10 +2,7 @@ import json
 
 
 def get_workshops():
-    with open('../data/workshops.json', 'r', encoding='utf-8') as f:
+    with open('data/workshops.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
 
     return json_data
-
-get_workshops = get_workshops()
-print(get_workshops)
